de5e3ac48e5a122e90c525bd1bcc922f9a98d2eb
Igor Ramnyonak <sloed95@gmail.com>
2024-02-02 11:25:39 +0300

    feat: intermediate build

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { ArticlesModule } from './articles/articles.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: 'postgres',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       synchronize: true,
       models: [],
     }),
