8c497c596af99cd848f4ea72bfb0395679af7ff5
ihar.ramnyonak <sloed95@gmail.com>
2024-08-06 18:42:13 +0300

    feat(year-matryca): calculation
    added calculation of year-matryca

--- src/calculator/years.service.ts
+++ src/calculator/years.service.ts
@@ -3,8 +3,8 @@ import { InjectModel } from '@nestjs/sequelize';
 import { Articles } from '../articles/articles.model';
 import { CalculatorService } from './calculator.service';
 import * as fs from 'fs';
-import { fieldsToReplace } from '../utils/fieldsToReplace';
 import { generatePdf } from '../utils/generatePdf';
+import { yearsFieldsToReplace } from '../utils/yearsFieldsToReplace';
 
 @Injectable()
 export class YearsService {
@@ -34,63 +34,45 @@ export class YearsService {
     return age;
   }
 
-  private choseFile(date: string): string {
-    const age = this.age(date);
-
-    let modifiedHtml: string;
-
-    switch (true) {
-      case age > 16 && age <= 30:
-        modifiedHtml = fs.readFileSync(
-          './src/assets/templates/years/16-30.html',
-          'utf-8',
-        );
-        break;
-      case age >= 31 && age <= 40:
-        modifiedHtml = fs.readFileSync(
-          './src/assets/templates/years/31-40.html',
-          'utf-8',
-        );
-        break;
-      case age >= 41 && age <= 50:
-        modifiedHtml = fs.readFileSync(
-          './src/assets/templates/years/41-50.html',
-          'utf-8',
-        );
-        break;
-      case age >= 51 && age <= 60:
-        modifiedHtml = fs.readFileSync(
-          './src/assets/templates/years/51-60.html',
-          'utf-8',
-        );
-        break;
-      case age >= 61 && age <= 70:
-        modifiedHtml = fs.readFileSync(
-          './src/assets/templates/years/61-70.html',
-          'utf-8',
-        );
-        break;
-      case age >= 71:
-        modifiedHtml = fs.readFileSync(
-          './src/assets/templates/years/71-80.html',
-          'utf-8',
-        );
-        break;
-      default:
-        modifiedHtml = 'No template available for the specified age range.';
-        break;
-    }
-
-    return modifiedHtml;
-  }
-
   public async fillHtmlYearsTemplate(date: string) {
-    let modifiedHtml = this.choseFile(date);
+    let modifiedHtml = fs.readFileSync(
+      './src/assets/templates/years/general-years.html',
+      'utf-8',
+    );
 
     const dateObject = await this.calculateService.calculate(date);
-    const fields = fieldsToReplace(dateObject);
 
-    for (const field of fields) {
+    const fields = yearsFieldsToReplace(dateObject);
+
+    const age = this.age(date).toString();
+
+    let index = fields.findIndex((item) => item.name === age);
+
+    if (index === -1) {
+      for (let i = parseInt(age, 10) - 1; i >= 0; i--) {
+        index = fields.findIndex((item) => item.name === i.toString());
+        if (index !== -1) {
+          break;
+        }
+      }
+    }
+
+    let newFields = [];
+
+    if (index === -1) {
+      // If age is less than the first available age, take the first 5 fields
+      newFields = fields.slice(0, 5);
+    } else if (
+      parseInt(age, 10) > parseInt(fields[fields.length - 1].name, 10)
+    ) {
+      // If age is greater than the last available age, take the last 5 fields
+      newFields = fields.slice(-5);
+    } else {
+      // Otherwise, take the 5 fields starting from the found index
+      newFields = fields.slice(index, index + 5);
+    }
+
+    for (const field of newFields) {
       const data = await this.calculateService.getDataFromDatabaseByParams(
         field.title,
         field.subtitle,
@@ -98,19 +80,29 @@ export class YearsService {
         this.articlesModel,
       );
 
-      modifiedHtml = modifiedHtml.replace(`{${field.name}Content}`, data);
-      modifiedHtml = modifiedHtml.replace(
-        `{${field.name}Number}`,
-        field.number,
-      );
-    }
-
-    for (const key in dateObject) {
-      if (dateObject.hasOwnProperty(key)) {
-        const regex = new RegExp(`{${key}}`, 'g');
-        modifiedHtml = modifiedHtml.replace(regex, dateObject[key]);
+      if (data) {
+        modifiedHtml = modifiedHtml.replace(`{${field.name}Content}`, data);
+        modifiedHtml = modifiedHtml.replace(
+          `{${field.name}Number}`,
+          `(${field.number})`,
+        );
+        modifiedHtml = modifiedHtml.replace(
+          `{${field.name}Scope}`,
+          field.scope,
+        );
+      } else {
+        // If no data, remove the entire block
+        const blockRegex = new RegExp(
+          `<h3>[^<]*${field.name}[^<]*LAT<\/h3>\\s*<p class="number">\\{${field.name}Number\\}<\/p>\\s*<p>\\{${field.name}Content\\}<\/p>`,
+          'g',
+        );
+        modifiedHtml = modifiedHtml.replace(blockRegex, '');
       }
     }
+
+    modifiedHtml = modifiedHtml.replace(/\{[0-9]+Number\}/g, '');
+    modifiedHtml = modifiedHtml.replace(/\{[0-9]+Content\}/g, '');
+    modifiedHtml = modifiedHtml.replace(/\{[0-9]+Scope\}/g, '');
 
     await generatePdf(modifiedHtml, `date-${date}`, true);
 
